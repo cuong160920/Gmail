abaa517b18c19e1e35463c52ddcb61db1ef9b112
Nguyen Manh Cuong <cuonga1k57@gmail.com>
2020-12-08 23:28:44 +0700

    Them 02/12

---
+++ app/src/main/java/com/example/gmailui/ItemAdapter.java
@@ -0,0 +1,133 @@
+package com.example.gmailui;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemAdapter extends BaseAdapter implements Filterable {
+    Context context;
+    List<ItemModel> items;
+    List<ItemModel> listfull;
+    private SearchFilter searchFilter;
+    public ItemAdapter(Context context, List<ItemModel> items) {
+        this.context = context;
+        this.items = items;
+        listfull = new ArrayList<>(items);
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        ViewHolder viewHolder;
+
+        if(view ==null){
+            view = LayoutInflater.from(context).inflate(R.layout.layout_item,viewGroup,false);
+            viewHolder = new ViewHolder();
+            viewHolder.sender =  view.findViewById(R.id.sender);
+            viewHolder.name = view.findViewById(R.id.name);
+            viewHolder.title = view.findViewById(R.id.title);
+            viewHolder.content = view.findViewById(R.id.content);
+            viewHolder.time = view.findViewById(R.id.time);
+            viewHolder.favorite = view.findViewById(R.id.favorite);
+            view.setTag(viewHolder);
+
+        }
+        else viewHolder = (ViewHolder) view.getTag();
+
+        ItemModel item = items.get(i);
+
+        viewHolder.sender.getBackground().setColorFilter(Color.parseColor(item.getMyHexColor()),PorterDuff.Mode.SRC_ATOP);
+        viewHolder.name.setText(item.getName());
+        viewHolder.sender.setText(String.valueOf(item.getName().charAt(0)));
+        viewHolder.title.setText(item.getTitle());
+        viewHolder.content.setText(item.getContent());
+        viewHolder.time.setText(item.getTime());
+        if(viewHolder.favorite.isChecked()) viewHolder.favorite.setButtonDrawable(  R.drawable.ic_baseline_star_24);
+        else viewHolder.favorite.setButtonDrawable(  R.drawable.ic_baseline_star_outline_24);
+        viewHolder.favorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                item.setFavorite(viewHolder.favorite.isChecked());
+                notifyDataSetChanged();
+            }
+        });
+        return view;
+
+    }
+
+    @Override
+    public Filter getFilter() {
+        if(searchFilter==null) {
+            searchFilter=new SearchFilter();
+        }
+        return searchFilter;
+    }
+    private class SearchFilter extends Filter{
+
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            FilterResults result = new FilterResults();
+            //listfull
+            if(constraint == null || constraint.length() == 0){
+                result.values = listfull;
+                result.values = listfull.size();
+            }else {
+                ArrayList<ItemModel> filterlist = new ArrayList<>();
+                for(ItemModel j: listfull ){
+                    if(j.getTitle().toLowerCase().contains(constraint) ||j.getName().contains(constraint)){
+                        filterlist.add(j);
+                    }
+                }
+                result.values = filterlist;
+                result.count = filterlist.size();
+            }
+            return result;
+        }
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            if(results.count == 0) {
+                notifyDataSetChanged();
+            }else {
+                items = (ArrayList<ItemModel>) results.values;
+                notifyDataSetChanged();
+            }
+        }
+    }
+
+    private class ViewHolder {
+        Drawable oval;
+        TextView sender;
+        TextView name;
+        TextView title;
+        TextView content;
+        TextView time;
+        CheckBox favorite;
+    }
+
+}
---
+++ app/src/main/java/com/example/gmailui/MainActivity.java
@@ -0,0 +1,150 @@
+package com.example.gmailui;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    List<ItemModel> items;
+    ListView listView;
+    ListView listView2;
+    String querySearch;
+    SearchView searchView;
+    ItemAdapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.v("TAG", "Oncreate");
+        items = new ArrayList<>();
+        items.add(new ItemModel("Cuong","Init","Hello! How are you ?","12:34AM","#eb7a34"));
+        items.add(new ItemModel("Andrew","the constract","we need to have a meeting at monday...","3:30PM","#a8eb34"));
+        items.add(new ItemModel("Jack","lorem sapbhies jdia aoies...","bisj js ofj eq j djoaid","2:24PM","#34eba8"));
+        items.add(new ItemModel("Sophia","Aliquam ullamcorper magna ","quis mollis felis interdum","6:30AM","#34c3eb"));
+        items.add(new ItemModel("Quisque eleifend","Aliquam varius turpis dolor, quis mollis","Nulla aliquet, dui quis eleifend placerat","8:18AM","#3462eb"));
+        items.add(new ItemModel("Anna","Lorem ipsum dolor sit amet","Convallis convallis tellus ","11:11AM","#a3ab59"));
+        items.add(new ItemModel("Adaline Reichel","Sed quis nisl sit amet neque laoreet finibus"," proin nibh nisl condimentum. ","12:03AM","#c2bdbc"));
+        items.add(new ItemModel("Roscoe Johns","Phasellus faucibus felis"," congue mi sit amet, convallis est.","2:33AM","#b734eb"));
+        items.add(new ItemModel("Keegan Thiel","Aliquam vel mauris fringilla","Ut ut lectus feugiat libero","7:31PM","#cf9988"));
+        items.add(new ItemModel("Ms. Karley Kiehn V","Morbi blandit nibh nec"," elit tristique semper.","4:16AM","#917ca1"));
+        items.add(new ItemModel("Emmett Lebsack","In luctus ligula eget nibh ","Et netus et malesuada","10:22PM","#ed5a73"));
+        items.add(new ItemModel("Gracie Weber","In aliquam sem fringilla","pharetra et ultrices neque.","6:20PM","#f0f0f5"));
+        items.add(new ItemModel("Max Coppola","Vitae tortor condimentum","labore et dolore magna ","10:20AM","#daa6e0"));
+
+         adapter = new ItemAdapter(this, items);
+         listView = findViewById(R.id.list_view);
+        listView.setAdapter(adapter);
+
+        registerForContextMenu(listView);
+        listView.setLongClickable(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+         searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Log.v("TAG", "Search with keyword: " + query);
+                querySearch = query;
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.v("TAG", "Keyword: " + newText);
+                    adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_favorite){
+            List<ItemModel> items2 = new ArrayList<>();
+                for (int i = 0; i < items.size(); i++){
+                    if(items.get(i).isFavorite()) {
+                        Log.v("favorite", "item" + i);
+                        items2.add(items.get(i));
+                        Log.v("add2favorite", "item" + items2.size());
+                    }
+                }
+                listView.setVisibility(View.GONE);
+                ItemAdapter adapter1 = new ItemAdapter(this, items2);
+                listView2 = findViewById(R.id.list_favorite);
+                listView2.setAdapter(adapter1);
+
+           return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo){
+        super.onCreateContextMenu(menu,v,menuInfo);
+        menu.setHeaderTitle("Select action");
+        menu.add(0,101, 0,"Details");
+        menu.add(0,102,0,"Delete");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item){
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+
+        if(item.getItemId() == 101){
+            go2Details(items.get(info.position));
+        }
+        return super.onContextItemSelected(item);
+    }
+    private void go2Details(ItemModel item){
+        Intent details = new Intent(MainActivity.this,Details.class);
+        //create Bundle
+        Bundle myBunder = new Bundle();
+        //add <key,value>
+        myBunder.putString("name",item.getName());
+        myBunder.putString("title",item.getTitle());
+        myBunder.putString("time",item.getTime());
+        myBunder.putString("content",item.getContent());
+        myBunder.putString("color",item.getMyHexColor());
+        myBunder.putBoolean("favorite",item.isFavorite());
+        //attatch the container to the intent
+        details.putExtras(myBunder);
+
+        startActivityForResult(details,10);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try{
+            if(requestCode == 10 && resultCode == Activity.RESULT_OK){
+                Log.v("TAG", "sucess");
+            }
+
+        }catch (Exception e){
+            Log.v("TAG", "KHONG MO DC INTENT");
+        }
+    }
+}
